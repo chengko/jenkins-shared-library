a3f294de7759690bf78503f1092d90288bad1732
cheng <cheng@7senses.com>
2024-01-17 02:08:34 +0800

    test7

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -1,6 +1,6 @@
 // vars/upload.groovy
 
-def uploadArtifacts(String projectName, String fromJob, String fromBuildNumber, String apkName) {
+def call(String projectName, String fromJob, String fromBuildNumber, String apkName) {
     // Your uploadArtifacts logic here
 
     def artifactLink = "<a href='https://${projectName}-artifacts.s3.amazonaws.com/APK/${apkName}.apk'>${apkName}.apk</a>"
