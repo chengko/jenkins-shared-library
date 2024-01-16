567e4f32bdad2691bf68bc8697c0e44668ff2187
cheng <cheng@7senses.com>
2024-01-17 01:57:49 +0800

    test4

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -2,9 +2,9 @@
 
 package common
 
-import static common.Project.NAME
+//import static common.Project.NAME
 
-def uploadArtifacts(String projectName = NAME, String fromJob, String fromBuildNumber, String APK_NAME) {
+def uploadArtifacts(String projectName, String fromJob, String fromBuildNumber, String APK_NAME) {
     // Your uploadArtifacts logic here
 
     def artifactLink = "<a href='https://${projectName}-artifacts.s3.amazonaws.com/APK/${APK_NAME}.apk'>${APK_NAME}.apk</a>"
