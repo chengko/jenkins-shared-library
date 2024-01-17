26a6ae805e02ecfa798e5fe3ce0922787d881105
cheng <cheng@7senses.com>
2024-01-17 11:26:59 +0800

    1

--- vars/instruction.groovy
+++ vars/instruction.groovy
@@ -1,7 +1,13 @@
 
 def uploadArtifacts(String projectName, String fromJob, String fromBuildNumber, String apkName) {
     // Your uploadArtifacts logic here
-
+    build job: 'Instruction/UploadArtifacts', parameters: [
+        string(name: 'projectName', value: projectName),
+        string(name: 'fromJob', value: fromJob),
+        string(name: 'buildNumber', value: fromBuildNumber),
+        string(name: 'archivePattern', value: "unity-andorid.apk"),
+        string(name: 'changeName', value: "${apkName}.apk"),
+        string(name: 'dir', value: "APK")]
     def artifactLink = "<a href='https://${projectName}-artifacts.s3.amazonaws.com/APK/${apkName}.apk'>${apkName}.apk</a>"
     currentBuild.description = artifactLink
 
