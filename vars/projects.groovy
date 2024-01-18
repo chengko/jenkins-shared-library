7a0b10ec52ee6308b9ee788d473a129d21a1163c
cheng <cheng@7senses.com>
2024-01-18 18:13:32 +0800

    1

--- vars/projects.groovy
+++ vars/projects.groovy
@@ -1,9 +1,9 @@
 // vars/project.groovy
 
-def load(String projectName) {
+def loadSettings(String projectName) {
     def content = libraryResource("project/${projectName}/settings.yml")
     def project = readYaml text: content
-    echo "load project: $project.Name"
+    echo "load project settings: $project.Name"
 
     return project
 }
