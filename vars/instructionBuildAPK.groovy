a205d529a711ab9b405d14976078df5e58411e40
cheng <cheng@7senses.com>
2024-01-24 11:35:12 +0800

    1

--- vars/instructionBuildAPK.groovy
+++ vars/instructionBuildAPK.groovy
@@ -1,8 +1,10 @@
 // vars/instructionBuildAPK.groovy
 
-def call(projectName, appConfig = 'android', buildTarget = 'Android', debug = false) {
+def call(projectName, bundleVersion, versionCode, buildEmbededAssets, appBundle, gitURL, branch, 
+    unityVersion, cleanWs, gitReset, archivePreviousBuild, deployMethod, archivePattern, apkName, customGradleVersion,
+    appConfig = 'android', buildTarget = 'Android', buildMethod = 'CITool.BuildApp', preprocess1 = '', preprocess2 = '', useIL2CPP = true, useApkExtension = false, debug = false) {
     
-    echo "${projectName}, ${appConfig}, ${buildTarget}, ${debug}"
+    echo "projectName: ${projectName}, appConfig:${appConfig}, gitURL:${gitURL}, buildTarget:${buildTarget}, debug:${debug}"
 }
 
 return this
