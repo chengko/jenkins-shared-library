3d097b64e1183754e99e10f23cf1c7d0a0a42c9d
cheng <cheng@7senses.com>
2024-01-24 11:00:45 +0800

    2

--- src/instruction1/Foo.groovy
+++ src/instruction1/Foo.groovy
@@ -2,13 +2,16 @@
 
 package instruction1
 
-class Foo implements Serializable {
+class Foo {
+
+    final Script script
+
+    Foo(Script script) {
+        this.script = script
+    }
 
     def myCustomFunction(String param1, String param2) {
-        script {
-            echo "Parameter 1: ${param1}"
-            echo "Parameter 2: ${param2}"
-        }
-        
+        script.echo "Parameter 1: ${param1}"
+        script.echo "Parameter 2: ${param2}"
     }
 }
