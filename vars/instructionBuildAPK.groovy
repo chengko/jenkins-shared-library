ace8718ac5ba232c1c3ea87bcf7b0bdfdc1279d8
cheng <cheng@7senses.com>
2024-01-24 11:49:42 +0800

    1

--- vars/instructionBuildAPK.groovy
+++ vars/instructionBuildAPK.groovy
@@ -2,9 +2,12 @@
 
 def call(projectName, bundleVersion, versionCode, buildEmbededAssets, appBundle, gitURL, branch, 
     unityVersion, cleanWs, gitReset, archivePreviousBuild, deployMethod, archivePattern, apkName, customGradleVersion,
-    appConfig = 'android', buildTarget = 'Android', buildMethod = 'CITool.BuildApp', preprocess1 = '', preprocess2 = '', useIL2CPP = true, useApkExtension = false, debug = false) {
+    appConfig = 'android', buildTarget = 'Android', buildMethod = 'CITool.BuildApp', preprocess1 = '', preprocess2 = '') {
     
     echo "projectName: ${projectName}, appConfig:${appConfig}, gitURL:${gitURL}, buildTarget:${buildTarget}, debug:${debug}"
 }
 
 return this
+
+
+                    
