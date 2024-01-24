5c4034e3e7ea29a4442497e261a02f9d8441c527
cheng <cheng@7senses.com>
2024-01-24 15:29:10 +0800

    1

--- src/com/makewish/BuildUnityArgs.groovy
+++ src/com/makewish/BuildUnityArgs.groovy
@@ -9,11 +9,12 @@ class BuildUnityArgs implements Serializable {
     Boolean debug = false
 
     BuildUnityArgs(Map args = [:]) {
-
+        println "Building..."
         args.each { key, value ->
             try {
                 this[key] = value
             } catch (MissingPropertyException e) {
+                
                 // Field does not exist in the object
                 println "Field '$key' does not exist in the object. Available fields: ${this.properties.keySet()}"
                 throw e
