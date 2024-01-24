4778bfa71f3c5959c90301311b1746cd0664c656
cheng <cheng@7senses.com>
2024-01-24 12:16:49 +0800

    5

--- vars/instructionBuildAPK.groovy
+++ vars/instructionBuildAPK.groovy
@@ -2,8 +2,8 @@
 
 def call(Map parameters = [:]) {
     def debug = parameters.debug ?: false
-    def useIL2CPP = parameters.useIL2CPP ?= true
-    def buildMethod = parameters.buildMethod ?= 'CITool.BuildApp'
+    def useIL2CPP = parameters.useIL2CPP ?: true
+    def buildMethod = parameters.buildMethod ?: 'CITool.BuildApp'
     echo "projectName: ${parameters.projectName}, bundleVersion:${parameters.bundleVersion}, gitURL:${parameters.gitURL}, buildTarget:${parameters.buildTarget}, parameters.il2cpp:${useIL2CPP}, il2cpp:${useIL2CPP}, parameters.debug:${parameters.debug}, debug:${debug}"
     //echo "projectName: ${projectName}, appConfig:${appConfig}, gitURL:${gitURL}, buildTarget:${buildTarget}, debug:${debug}"
 }
