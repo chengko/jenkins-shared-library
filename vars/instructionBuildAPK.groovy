4b38f1f369bd61149d9f563838382f9dccfc0ffa
cheng <cheng@7senses.com>
2024-01-24 12:02:00 +0800

    4

--- vars/instructionBuildAPK.groovy
+++ vars/instructionBuildAPK.groovy
@@ -1,6 +1,6 @@
 // vars/instructionBuildAPK.groovy
 
-def call(projectName, bundleVersion = '1.0.0') {
+def call(projectName = 'something', bundleVersion = '1.0.0') {
     echo "projectName: ${projectName}, bundleVersion:${bundleVersion}"
     //echo "projectName: ${projectName}, appConfig:${appConfig}, gitURL:${gitURL}, buildTarget:${buildTarget}, debug:${debug}"
 }
